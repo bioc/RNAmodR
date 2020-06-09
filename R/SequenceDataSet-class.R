fcc95e57caf971f24d17c75ce2217b2bb0b504e2
Hervé Pagès <hpages@fredhutch.org>
2020-06-08 17:55:15 -0700

    resync with S4Vectors 0.27.12

--- R/SequenceDataSet-class.R
+++ R/SequenceDataSet-class.R
@@ -85,9 +85,9 @@ setMethod("show", "SequenceDataSet",
             print(out, quote = FALSE, right = TRUE)
           })
 
-# vertical_slot_names ----------------------------------------------------------
+# parallel_slot_names ----------------------------------------------------------
 #' @rdname RNAmodR-internals
-setMethod("vertical_slot_names", "SequenceDataSet",
+setMethod("parallel_slot_names", "SequenceDataSet",
           function(x) c("listData", callNextMethod())
 )
 
